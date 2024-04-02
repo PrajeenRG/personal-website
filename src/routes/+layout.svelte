e59a6a52e85a5b113ddb0295e57849b7ada8582b
Prajeen Govardhanam <contact@prajeen.com>
2024-04-02 20:46:08 +0530

    :lipstick: Improve text bg color contrast

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -26,24 +26,30 @@
 	];
 </script>
 
-<nav class="absolute top-0 z-50 flex h-16 w-screen place-items-center justify-between bg-white px-16 dark:bg-stone-900">
+<nav
+	class="absolute top-0 z-50 flex h-16 w-screen place-items-center justify-between bg-white px-16 dark:bg-stone-900"
+>
 	<a href="/">
-		<enhanced:img src="/src/lib/assets/logo.png?w=128;64;32" alt="Prajeen Govardhanam" class="h-8 w-8" />
+		<enhanced:img
+			src="/src/lib/assets/logo.png?w=128;64;32"
+			alt="Prajeen Govardhanam"
+			class="h-8 w-8"
+		/>
 	</a>
-	<div class="flex gap-4 text-sm md:text-base font-semibold">
+	<div class="flex gap-4 text-sm font-semibold md:text-base">
 		{#each navigations as { slug, name }}
 			<a
 				href={slug}
 				class={$page.url.pathname == slug
-					? 'text-stone-600 dark:text-stone-400'
-					: 'text-stone-400 dark:text-stone-600'}>{name}</a
+					? 'text-stone-800 dark:text-stone-200'
+					: 'text-stone-600 dark:text-stone-400'}>{name}</a
 			>
 		{/each}
 	</div>
 </nav>
 <slot />
 <footer
-	class="flex flex-col place-items-center justify-evenly border-t-2 border-stone-200 bg-white px-8 py-2 text-stone-500 dark:border-stone-700 dark:bg-stone-900 dark:text-stone-400"
+	class="flex flex-col place-items-center justify-evenly border-t-2 border-stone-200 bg-white px-8 py-2 text-stone-600 dark:border-stone-700 dark:bg-stone-900 dark:text-stone-300"
 >
 	<p class="text-sm md:text-base">© 2024 Prajeen Govardhanam</p>
 	<p class="text-xs md:text-sm">Powered by SvelteKit</p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,17 @@
 <svelte:head>
 	<title>Prajeen Govardhanam</title>
 </svelte:head>
-<div class="grid w-screen h-[98vh] place-items-center bg-white dark:bg-stone-900">
+<div class="grid h-[98vh] w-screen place-items-center bg-white dark:bg-stone-900">
 	<div class="flex flex-col place-items-center">
 		<enhanced:img
 			src="/src/lib/assets/display-picture.png?w=512;256;128"
 			alt="Prajeen Govardhanam"
-			class="h-48 w-48 md:h-64 md:w-64 rounded-full mb-4"
+			class="mb-4 h-48 w-48 rounded-full md:h-64 md:w-64"
 		/>
-		<p class="name name-gradient text-transparent text-3xl md:text-5xl">Prajeen Govardhanam</p>
-		<p class="tagline text-stone-900 dark:text-stone-300 text-lg md:text-2xl">Committing Code and Spreading Memes</p>
+		<p class="name name-gradient text-3xl text-transparent md:text-5xl">Prajeen Govardhanam</p>
+		<p class="tagline text-lg text-stone-900 md:text-2xl dark:text-stone-300">
+			Committing Code and Spreading Memes
+		</p>
 	</div>
 </div>
 
