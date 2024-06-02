015ab9902284836f8e69e71adbcbf09ca854073c
Prajeen Govardhanam <contact@prajeen.com>
2024-06-02 20:01:37 +0530

    :bento: Update display picture
    Signed-off-by: Prajeen Govardhanam <contact@prajeen.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,9 @@
 <div class="grid h-[98vh] w-screen place-items-center bg-white dark:bg-stone-900">
 	<div class="flex flex-col place-items-center">
 		<enhanced:img
-			src="/src/lib/assets/display-picture.png?w=512;256;128"
+			src="/src/lib/assets/prajeen.jpg?w=512;256;128"
 			alt="Prajeen Govardhanam"
-			class="mb-4 h-48 w-48 rounded-full md:h-64 md:w-64"
+			class="mb-4 h-48 w-48 rounded-full border-4 border-orange-600 shadow-orange-800 shadow-sm md:shadow-md md:h-64 md:w-64"
 		/>
 		<p class="name name-gradient text-3xl text-transparent md:text-5xl">Prajeen Govardhanam</p>
 		<p class="tagline text-lg text-stone-900 md:text-2xl dark:text-stone-300">
