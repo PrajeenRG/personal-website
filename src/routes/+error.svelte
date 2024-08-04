a890c8879087ec72eea6044e11f59f0e51d5bc4c
Prajeen Govardhanam <p@prajeen.com>
2024-08-04 18:34:47 +0000

    :sparkles: Add default error page and blog placeholder
    Signed-off-by: Prajeen Govardhanam <p@prajeen.com>

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { page } from '$app/stores'
+  import { name } from '$lib/data.json'
+</script>
+
+<svelte:head>
+  <title>{name} | {$page.status} {$page.error?.message}</title>
+</svelte:head>
+<div class="min-h-[88vh] grid place-items-center">
+  <div class="flex flex-col gap-3 place-items-center">
+    <h1 class="font-bold text-5xl">Error {$page.status}</h1>
+    <p class="text-xl">{$page.error?.message}</p>
+  </div>
+</div>
---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { name } from '$lib/data.json'
+</script>
+
+<svelte:head>
+  <title>{name} | Blog</title>
+  <meta
+    name="description"
+    content="A blog site that focus on tech and general rants"
+  />
+</svelte:head>
+<div class="min-h-[88vh] grid place-items-center">
+  <div class="flex flex-col gap-3 place-items-center">
+    <h1 class="font-bold text-5xl">Blog site is under construction</h1>
+    <p class="text-xl">Please stay tuned for further updates</p>
+  </div>
+</div>
